ca29a8711ba30794ce6d85cc1715f7f7adb21423
zw6666 <2714709357@qq.com>
2019-01-10 17:11:34 +0800

    111

--- yazhoubang/src/main/resources/static/js/data-tables.js
+++ yazhoubang/src/main/resources/static/js/data-tables.js
@@ -53,26 +53,12 @@ var table=$('#datatable').DataTable({
 
 	$('#datatable tbody').on('click', 'tr', function () {
 
-		var data = table.row( this ).data();
-		var postData={"where":1,
-			"c_id":data[2],};
+        var data = table.row( this ).data();
+        var postData={"where":1,
+            "c_id":data[2],}
 
-		$.ajax(
-			{
-				type:"POST",
-				url:"/PDetailTable",
-				data:postData,
-				dataType: "json",
-				async:false,
-				success:function (result) {
-					//这句要写成一行
-				},
-				error:function () {
-					console.log("fail");
-				}
-			}
-		)
-		window.open ('PDetailTable');
+        var link="/PDetailTable/"+data[2];
+        window.open (link);
 	} );
 
 function addButton() {
